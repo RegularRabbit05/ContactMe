4c47b5f0d3b65965e8fe84e5a09670e685689f50
RegularRabbit05 <69511985+RegularRabbit05@users.noreply.github.com>
2024-11-28 18:11:44 +0100

    Removed unnecessary library

--- api/sendMessage.go
+++ api/sendMessage.go
@@ -3,13 +3,10 @@ package api
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/wneessen/go-mail"
-	"log"
+	mailer "github.com/wneessen/go-mail"
 	"net/http"
+	"net/mail"
 	"os"
-
-	"github.com/go-email-validator/go-email-validator/pkg/ev"
-	"github.com/go-email-validator/go-email-validator/pkg/ev/evmail"
 )
 
 func SendMessage(w http.ResponseWriter, r *http.Request) {
@@ -32,61 +29,55 @@ Message from: %s (%s)
 		return
 	}
 
-	defer func() {
-		if r := recover(); r != nil {
-		}
-	}()
-
 	type Mail struct {
 		Origin string `json:"from"`
 		Title  string `json:"subject"`
 		Body   string `json:"body"`
 	}
 
-	e := func() {
-		var m Mail
-		ip := "IP:" + r.RemoteAddr + "|HEADER:" + r.Header.Get("X-Forwarded-For")
-		err := json.NewDecoder(r.Body).Decode(&m)
-		if err != nil || m.Origin == "" || m.Title == "" || m.Body == "" {
-			http.Error(w, "Bad request", http.StatusBadRequest)
-			return
-		}
-		if !ev.NewSyntaxValidator().Validate(ev.NewInput(evmail.FromString(m.Origin))).IsValid() {
-			http.Error(w, "Bad request", http.StatusBadRequest)
-			return
-		}
-
-		body := fmt.Sprintf(messageTemplate, m.Origin, ip, m.Body)
-
-		message := mail.NewMsg()
-		if err = message.From(os.Getenv("VAR_SMTP_FROM")); err != nil {
-			http.Error(w, "Unable to send mail", http.StatusInternalServerError)
-			log.Panicln(err)
-			return
-		}
-		if err = message.To(os.Getenv("VAR_SMTP_TO")); err != nil {
-			http.Error(w, "Unable to send mail", http.StatusInternalServerError)
-			log.Panicln(err)
-			return
-		}
-
-		message.Subject(m.Title)
-		message.SetBodyString(mail.TypeTextPlain, body)
-		client, err := mail.NewClient(os.Getenv("VAR_SMTP_SERVER"), mail.WithTLSPortPolicy(mail.TLSOpportunistic), mail.WithSMTPAuth(mail.SMTPAuthPlain),
-			mail.WithUsername(os.Getenv("VAR_SMTP_USER")), mail.WithPassword(os.Getenv("VAR_SMTP_PASSWORD")))
-		if err != nil {
-			http.Error(w, "Connection failure", http.StatusBadGateway)
-			log.Panicln(err)
-			return
-		}
-
-		if err = client.DialAndSend(message); err != nil {
-			http.Error(w, "Unable to send mail", http.StatusBadGateway)
-			log.Panicln(err)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
+	var m Mail
+	ip := "IP:" + r.RemoteAddr + "|HEADER:" + r.Header.Get("X-Forwarded-For")
+	err := json.NewDecoder(r.Body).Decode(&m)
+	if err != nil || m.Origin == "" || m.Title == "" || m.Body == "" {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
 	}
-	e()
+
+	valid := func(email string) bool {
+		_, err := mail.ParseAddress(email)
+		return err == nil
+	}
+
+	if !valid(m.Origin) {
+		http.Error(w, "Invalid sender", http.StatusBadRequest)
+		return
+	}
+
+	body := fmt.Sprintf(messageTemplate, m.Origin, ip, m.Body)
+
+	message := mailer.NewMsg()
+	if err = message.From(os.Getenv("VAR_SMTP_FROM")); err != nil {
+		http.Error(w, "Unable to send mail", http.StatusInternalServerError)
+		return
+	}
+	if err = message.To(os.Getenv("VAR_SMTP_TO")); err != nil {
+		http.Error(w, "Unable to send mail", http.StatusInternalServerError)
+		return
+	}
+
+	message.Subject(m.Title)
+	message.SetBodyString(mailer.TypeTextPlain, body)
+	client, err := mailer.NewClient(os.Getenv("VAR_SMTP_SERVER"), mailer.WithTLSPortPolicy(mailer.TLSOpportunistic), mailer.WithSMTPAuth(mailer.SMTPAuthPlain),
+		mailer.WithUsername(os.Getenv("VAR_SMTP_USER")), mailer.WithPassword(os.Getenv("VAR_SMTP_PASSWORD")))
+	if err != nil {
+		http.Error(w, "Connection failure", http.StatusBadGateway)
+		return
+	}
+
+	if err = client.DialAndSend(message); err != nil {
+		http.Error(w, "Unable to send mail", http.StatusBadGateway)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
--- go.mod
+++ go.mod
@@ -2,51 +2,9 @@ module ContactMe
 
 go 1.23
 
-require (
-	github.com/go-email-validator/go-email-validator v0.0.0-20230409163946-b8b9e6a0552e
-	github.com/wneessen/go-mail v0.5.2
-)
+require github.com/wneessen/go-mail v0.5.2
 
 require (
-	github.com/FGRibreau/mailchecker/v4 v4.1.19 // indirect
-	github.com/XiaoMi/pegasus-go-client v0.0.0-20210427083443-f3b6b08bc4c2 // indirect
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b // indirect
-	github.com/cenkalti/backoff/v4 v4.1.0 // indirect
-	github.com/cespare/xxhash/v2 v2.1.1 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/eko/gocache v1.2.0 // indirect
-	github.com/emirpasic/gods v1.18.1 // indirect
-	github.com/go-redis/redis/v8 v8.8.2 // indirect
-	github.com/golang/mock v1.6.0 // indirect
-	github.com/golang/protobuf v1.4.3 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pegasus-kv/thrift v0.13.0 // indirect
-	github.com/prometheus/client_golang v1.11.0 // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.26.0 // indirect
-	github.com/prometheus/procfs v0.6.0 // indirect
-	github.com/sethvargo/go-password v0.2.0 // indirect
-	github.com/sirupsen/logrus v1.7.0 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
-	github.com/tevino/abool v1.2.0 // indirect
-	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
-	go.opentelemetry.io/otel v0.19.0 // indirect
-	go.opentelemetry.io/otel/metric v0.19.0 // indirect
-	go.opentelemetry.io/otel/trace v0.19.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.26.0-rc.1 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
-	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637 // indirect
-	h12.io/socks v1.0.3 // indirect
-	k8s.io/apimachinery v0.0.0-20191123233150-4c4803ed55e3 // indirect
 )
